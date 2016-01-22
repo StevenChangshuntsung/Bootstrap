43abe067a74ef097affc68a23d6ef2af11afd393
StevenChang <cstworkout@shuntsungchang.com>
2016-01-22 10:37:44 +0800

    First Bootstrap 管理平台

---
+++ pages/js/purview.js
@@ -0,0 +1,13 @@
+//test
+//sessionStorage.lastname="123";
+//document.write(sessionStorage.lastname);
+var dt = new Date();
+//var time = dt.getHours() +"."+ dt.getMinutes()
+var time = dt.getHours() +".00"
+if (sessionStorage.lastname != time){
+	alert('請重新登入！');
+	document.write("<body onload=javascript:window.location.href='./login.html';>");
+} 
+// if (sessionStorage.lastname != "123"){
+// 	window.stop ? window.stop() : document.execCommand("Stop");
+// }
