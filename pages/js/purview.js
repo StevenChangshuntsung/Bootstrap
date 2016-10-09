8be78d1fbd2c84b850781012dd7b5ab08c1b8ad7
CSTpc <cstworkout@shuntsungchang.com>
2016-10-10 06:08:53 +0800

    20161010
    C:\htdocs\Bootstrap\pages\js\purview.js

--- pages/js/purview.js
+++ pages/js/purview.js
@@ -5,7 +5,7 @@ var dt = new Date();
 //var time = dt.getHours() +"."+ dt.getMinutes()
 var time = dt.getHours() +".00"
 // if (sessionStorage.lastname != time){
-// 	// alert('請重新登入！');
+// 	alert('請重新登入！');
 // 	document.write("<body onload=javascript:window.location.href='./login.html';>");
 // } 
 // if (sessionStorage.lastname != "123"){
