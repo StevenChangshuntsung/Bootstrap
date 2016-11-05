a8524eb99408e9fcf86777cbafb1667558dc3d69
CSTpc <cstworkout@shuntsungchang.com>
2016-11-05 18:41:43 +0800

    test

--- pages/inc/websetup.php
+++ pages/inc/websetup.php
@@ -3,7 +3,7 @@
  * webs
  */
 $conn_dbSource = array("host" => "mysql.hostinger.com.hk",
-        "username" => " u896825658_root", "password" => "23480956",
+        "username" => "u896825658_root", "password" => "23480956",
         "dbname" => "u896825658_test",
         "port" => "3306", "socket" => "");
 // $conn_dbSource = array("host" => "127.0.0.1",
@@ -13,4 +13,7 @@ $conn_dbSource = array("host" => "mysql.hostinger.com.hk",
 
 
 
+
+
+	// u896825658_test	u896825658_root	mysql.hostinger.com.hk
 ?>
